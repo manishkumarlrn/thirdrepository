e5c411336e395d4190687495fb1ad3de4b059b41
manishkumarlrn <manish.kumar@lrn.com>
2023-03-06 12:10:35 +0530

    text.js added

---
+++ script/text.js
@@ -0,0 +1,16 @@
+//Below config is to turn waitforaudio(audiolock) on/off by setting the value true/false
+
+waitForAudio=false;
+
+enableCourseAudio=true;
+
+forceNavigation=true;
+
+// tap click popup
+enableTapPopupDesktop=false;
+enableTapPopupMobile=false;
+branding = true;
+
+//Orientation Flag (set "true" to enable and "false" to disable)
+phoneLandscapeFlg = false;
+tabletPortraitFlg = false;
